9b0f4ccb13b632951aacd7ea2a009176d1f15bc4
PrakharChandrakar383 <110602995+PrakharChandrakar383@users.noreply.github.com>
2022-08-04 16:49:24 +0530

    04-08-2022 strlen function in c

---
+++ strlen_code.c
@@ -0,0 +1,12 @@
+//program to impliment strlen() function using c
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+char s[40];
+int l;
+printf("Enter any string:");
+scanf("%s",&s);
+l = strlen(s);
+printf("The Length of string is %d",l);
+}
